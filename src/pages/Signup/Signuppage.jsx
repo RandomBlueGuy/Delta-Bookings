addbbb77446b1a5fe8742ded6da8fe356d2d1569
RandomBlueGuy <randombluemail@gmail.com>
2023-02-28 11:15:50 -0500

    xxxx

--- src/pages/Signup/Signuppage.jsx
+++ src/pages/Signup/Signuppage.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Signup from './Signup'
-import NavBar from '../UniversalComponents/NavBar'
-import UpButton from '../UniversalComponents/UpButton'
-import Footer from '../UniversalComponents/Footer'
-import HomeHeader from './HeaderSignup'
+import React from "react";
+import Signup from "./Signup";
+import NavBar from "../UniversalComponents/NavBar";
+import UpButton from "../UniversalComponents/UpButton";
+import Footer from "../UniversalComponents/Footer";
+import HomeHeader from "./HeaderSignup";
+
 
 function Signuppage() {
   return (
-      <div>
-          <NavBar />
-          <HomeHeader />
-          <UpButton />
-          <Signup />
-          <Footer />
+    <div>
+      <NavBar />
+      <HomeHeader />
+      <UpButton />
+        <Signup />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Signuppage
+export default Signuppage;
