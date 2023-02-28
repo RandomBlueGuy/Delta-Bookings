cd3cdf5ee47daf6ddb3a22877abd8755c40ed67d
MSaenz1011 <michaelsaenz96@hotmail.com>
2023-02-28 13:10:46 -0500

    added components, Install Bootstrap and added it to the app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Home/Homepage'
-import HotelList from './pages/HotelList/HotelListpage'
-import Signuppage from './pages/Signup/Signuppage';
-import './App.css';
-import './pages/UniversalComponents/BaseStyles.css'
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Home/Homepage";
+import HotelList from "./pages/HotelList/HotelListpage";
+import Signuppage from "./pages/Signup/Signuppage";
+import "./App.css";
+import "./pages/UniversalComponents/BaseStyles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
---
+++ src/pages/Bookings-Checkouts/BookingInfo.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./styles.css";
+
+export default function BookingModule() {
+  return (
+    <article className="container-1">
+      <section className="info-container">
+        <h6>Booking Summary</h6>
+        <section className="first-container d-flex justify-content-start align-items-center">
+          <img
+            src="https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI="
+            alt=""
+            className="picture-example"
+          />
+          <div className="hotel-info">
+            <strong className="hotel-name">Hotel Name</strong>
+            <p>Hotel Location</p>
+          </div>
+        </section>
+
+        <section className="second-container">
+          <div className="d-flex justify-content-between">
+            <p>Check In</p>
+            <p>Check Out</p>
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <p>18 Sept</p>
+            <p>22 Sept </p>
+          </div>
+
+          <div className="checkout-in d-flex justify-content-between">
+            <p>Check In Time</p>
+            <p>Check Out Time</p>
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <p>10:30</p>
+            <p>11:30</p>
+          </div>
+        </section>
+
+        <section className="third-container d-flex justify-content-between">
+          <p>Number of Nights</p>
+          <p>Edit</p>
+        </section>
+
+        <section className="fourth-container">
+          <h5>Payment Details</h5>
+
+          <div className="d-flex justify-content-between">
+            <p>Base Price</p>
+            <p>Price</p>
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <p>Promo Discount</p>
+            <p>Promo</p>
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <p>Tax & Services Fees</p>
+            <p>Tax</p>
+          </div>
+        </section>
+
+        <section className="fifth-container d-flex justify-content-between">
+          <p>Payable Amount</p>
+          <p>Amount</p>
+        </section>
+      </section>
+    </article>
+  );
+}
