065dfc5219dccfb355cd40a7ba4778c7e5a1a3e7
MSaenz1011 <michaelsaenz96@hotmail.com>
2023-03-08 18:44:32 -0500

    added info extraction por Traveller

--- src/pages/Bookings-Checkouts/TravellerInfo.jsx
+++ src/pages/Bookings-Checkouts/TravellerInfo.jsx
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function TravellerInfo() {
+  const [info, setInfo] = useState({
+    firstname: "",
+    lastname: "",
+    inputEmail: "",
+    contactInfo: 0,
+    request: "",
+    coupon: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value, type } = event.target;
+    setInfo({
+      ...info,
+      [name]: value,
+    });
+    console.log(info);
+  };
+
+  //const handleInfo = (event) => {};
+
+  const { firstname, lastname, inputEmail, contactInfo, request, coupon } =
+    info;
+
   return (
     <article className="container-2">
       <section className="travelInfo-container">
         <h4>Traveller Information</h4>
         <div className="person-info" role="form">
           <div class="name-info">
-            <label for="firstname">
+            <label htmlFor="firstname">
               First Name
               <input
+                name="firstname"
                 type="text"
                 class="form-control"
                 id="firstname"
                 placeholder="First Name"
+                onChange={(event) => handleChange(event)}
+                value={firstname}
               />
             </label>
           </div>
           <div class="lastname-info">
-            <label for="lastname">
+            <label htmlFor="lastname">
               Last Name
               <input
+                name="lastname"
                 type="text"
                 class="form-control"
                 id="lastname"
+                onChange={(event) => handleChange(event)}
                 placeholder="Last Name"
+                value={lastname}
               />
             </label>
           </div>
@@ -33,14 +62,17 @@ export default function TravellerInfo() {
 
         <section className="email-group">
           <div class="email-info">
-            <label for="exampleInputEmail1">
+            <label htmlFor="inputEmail">
               Email address
               <input
+                name="inputEmail"
                 type="email"
                 class="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
+                onChange={(event) => handleChange(event)}
                 placeholder="Enter email"
+                value={inputEmail}
               />
             </label>
             <small id="emailHelp" class="form-text text-muted">
@@ -51,39 +83,48 @@ export default function TravellerInfo() {
 
         <section className="contact-info">
           <div class="form-group">
-            <label for="contactInfo">
+            <label htmlFor="contactInfo">
               Contact Info
               <input
+                name="contactInfo"
                 type="tel"
                 class="form-control"
                 id="exampleInputEmail1"
+                onChange={(event) => handleChange(event)}
                 placeholder="+57"
+                value={contactInfo}
               />
             </label>
           </div>
         </section>
 
         <section className="request-info">
-          <label for="Request">
+          <label htmlFor="request">
             Special Request
             <textarea
-              maxlength="120"
+              name="request"
+              maxlength="60"
               class="form-control"
               id="exampleFormControlTextarea1"
+              onChange={(event) => handleChange(event)}
               rows="2"
               placeholder="E.g: Transport fee"
+              value={request}
             ></textarea>
           </label>
         </section>
 
         <section className="coupon-info">
-          <label for="coupon">Have A Coupon Code?</label>
+          <label htmlFor="coupon">Have A Coupon Code?</label>
           <div className="coupon-wrapper">
             <input
+              name="coupon"
               type="text"
               class="form-control"
               placeholder="Promo Code"
+              onChange={(event) => handleChange(event)}
               aria-describedby="basic-addon2"
+              value={coupon}
             />
             <div class="input-group-append">
               <button type="submit" className="btn">
@@ -94,7 +135,7 @@ export default function TravellerInfo() {
         </section>
 
         <section className="button-pay">
-          <button type="button" className="btn">
+          <button type="submit" className="btn">
             PAY NOW
           </button>
         </section>
