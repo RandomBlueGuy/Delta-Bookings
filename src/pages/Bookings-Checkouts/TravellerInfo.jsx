b9c4d0c71ca0a14bf9e89c366bb3b47e73e1f7c9
MSaenz1011 <michaelsaenz96@hotmail.com>
2023-03-26 12:12:54 -0500

    added initial version of post sending

--- src/pages/Bookings-Checkouts/TravellerInfo.jsx
+++ src/pages/Bookings-Checkouts/TravellerInfo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./styles.css";
 
 export default function TravellerInfo() {
@@ -58,127 +59,151 @@ export default function TravellerInfo() {
     }
 
     setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", {
+          firstname,
+          lastname,
+          inputEmail,
+          contactInfo,
+          request,
+          coupon,
+        })
+        .then((response) => console.log(response.data))
+        .catch((error) => console.error(error));
+
+      setInfo({
+        firstname: "",
+        lastname: "",
+        inputEmail: "",
+        contactInfo: "",
+        request: "",
+        coupon: "",
+      });
+      setErrors({});
+    }
   };
 
   return (
-    <form onSubmit={handleInfo} className="container-2">
-      <section className="travelInfo-container">
+    <form onSubmit={handleInfo} className='container-2'>
+      <section className='travelInfo-container'>
         <h4>Traveller Information</h4>
-        <div className="person-info" role="form">
-          <div class="name-info">
-            <label htmlFor="firstname">
+        <div className='person-info' role='form'>
+          <div class='name-info'>
+            <label htmlFor='firstname'>
               First Name
               <input
-                name="firstname"
-                type="text"
-                class="form-control"
-                id="firstname"
-                placeholder="First Name"
+                name='firstname'
+                type='text'
+                class='form-control'
+                id='firstname'
+                placeholder='First Name'
                 onChange={(event) => handleChange(event)}
                 value={firstname}
               />
               {errors.firstName && (
-                <span className="error">{errors.firstName}</span>
+                <span className='error'>{errors.firstName}</span>
               )}
             </label>
           </div>
-          <div class="lastname-info">
-            <label htmlFor="lastname">
+          <div class='lastname-info'>
+            <label htmlFor='lastname'>
               Last Name
               <input
-                name="lastname"
-                type="text"
-                class="form-control"
-                id="lastname"
+                name='lastname'
+                type='text'
+                class='form-control'
+                id='lastname'
                 onChange={(event) => handleChange(event)}
-                placeholder="Last Name"
+                placeholder='Last Name'
                 value={lastname}
               />
               {errors.lastName && (
-                <span className="error">{errors.lastName}</span>
+                <span className='error'>{errors.lastName}</span>
               )}
             </label>
           </div>
         </div>
 
-        <section className="email-group">
-          <div class="email-info">
-            <label htmlFor="inputEmail">
+        <section className='email-group'>
+          <div class='email-info'>
+            <label htmlFor='inputEmail'>
               Email address
               <input
-                name="inputEmail"
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                name='inputEmail'
+                type='email'
+                class='form-control'
+                id='exampleInputEmail1'
+                aria-describedby='emailHelp'
                 onChange={(event) => handleChange(event)}
-                placeholder="Enter email"
+                placeholder='Enter email'
                 value={inputEmail}
               />
-              {errors.email && <span className="error">{errors.email}</span>}
+              {errors.email && <span className='error'>{errors.email}</span>}
             </label>
-            <small id="emailHelp" class="form-text text-muted">
+            <small id='emailHelp' class='form-text text-muted'>
               Booking confirmation will be sent to this email ID.
             </small>
           </div>
         </section>
 
-        <section className="contact-info">
-          <div class="form-group">
-            <label htmlFor="contactInfo">
+        <section className='contact-info'>
+          <div class='form-group'>
+            <label htmlFor='contactInfo'>
               Contact Info
               <input
-                name="contactInfo"
-                type="tel"
-                class="form-control"
-                id="exampleInputEmail1"
+                name='contactInfo'
+                type='tel'
+                class='form-control'
+                id='exampleInputEmail1'
                 onChange={(event) => handleChange(event)}
-                placeholder="+57"
+                placeholder='+57'
                 value={contactInfo}
               />
-              {errors.number && <span className="error">{errors.number}</span>}
+              {errors.number && <span className='error'>{errors.number}</span>}
             </label>
           </div>
         </section>
 
-        <section className="request-info">
-          <label htmlFor="request">
+        <section className='request-info'>
+          <label htmlFor='request'>
             Special Request
             <textarea
-              name="request"
-              maxlength="60"
-              class="form-control"
-              id="exampleFormControlTextarea1"
+              name='request'
+              maxlength='60'
+              class='form-control'
+              id='exampleFormControlTextarea1'
               onChange={(event) => handleChange(event)}
-              rows="2"
-              placeholder="E.g: Transport fee"
+              rows='2'
+              placeholder='E.g: Transport fee'
               value={request}
             ></textarea>
           </label>
         </section>
 
-        <section className="coupon-info">
-          <label htmlFor="coupon">Have A Coupon Code?</label>
-          <div className="coupon-wrapper">
+        <section className='coupon-info'>
+          <label htmlFor='coupon'>Have A Coupon Code?</label>
+          <div className='coupon-wrapper'>
             <input
-              name="coupon"
-              type="text"
-              class="form-control"
-              placeholder="Promo Code"
+              name='coupon'
+              type='text'
+              class='form-control'
+              placeholder='Promo Code'
               onChange={(event) => handleChange(event)}
-              aria-describedby="basic-addon2"
+              aria-describedby='basic-addon2'
               value={coupon}
             />
-            <div class="input-group-append">
-              <button type="submit" className="btn">
+            <div class='input-group-append'>
+              <button type='submit' className='btn'>
                 APPLY
               </button>
             </div>
           </div>
         </section>
 
-        <section className="button-pay">
-          <button type="submit" className="btn">
+        <section className='button-pay'>
+          <button type='submit' className='btn'>
             PAY NOW
           </button>
         </section>
--- src/pages/HotelList/SearchBar.jsx
+++ src/pages/HotelList/SearchBar.jsx
@@ -1,33 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
+  const [info, setInfo] = useState({
+    city: "",
+    datein: "",
+    dateout: "",
+    guestnumber: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const { city, datein, dateout, guestnumber } = info;
+  const start = new Date(datein);
+  const end = new Date(guestnumber);
+  const today = new Date();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInfo({
+      ...info,
+      [name]: value,
+    });
+  };
+
+  const handleInfo = (event) => {
+    event.preventDefault();
+    const validationErrors = {};
+
+    if (city.trim() === "") {
+      validationErrors.firstName = "Please enter a city";
+    } else if (!/^[a-zA-Z]+$/.test(city)) {
+      validationErrors.cityName = "city name must only contain numbers";
+    }
+
+    // Validation if Date In is bigger than End Date
+    if (start.getDate() > end.getDate()) {
+      validationErrors.dateRange = "Invalid date range";
+      //|| end < start
+    } else if (start < today) {
+      validationErrors.dateRange = "The inital or end date is expired";
+    }
+
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      // Envio de info
+      setErrors({});
+    }
+  };
+
   return (
-      <form className="SearchBar-ctn" action="">
-        <div className="form-box-ctn">
-          <label htmlFor="">HOTEL</label>
-          <input type="text" placeholder="Ingrese ciudad de destino" />
-        </div>
+    <form onSubmit={handleInfo} className='SearchBar-ctn' action=''>
+      <div className='form-box-ctn'>
+        <label htmlFor='city'>HOTEL</label>
+        <input
+          name='city'
+          type='text'
+          placeholder='Ingrese ciudad de destino'
+          onChange={(event) => handleChange(event)}
+          value={city}
+        />
+      </div>
 
-        <div className="form-box-ctn">
-          <label htmlFor="">CHECK-IN</label>
-          <input type="date" placeholder="Fecha del Check-in" />
-        </div>
+      <div className='form-box-ctn'>
+        <label htmlFor='datein'>CHECK-IN</label>
+        <input
+          name='datein'
+          type='date'
+          placeholder='Fecha del Check-in'
+          onChange={(event) => handleChange(event)}
+          value={datein}
+        />
+      </div>
 
-        <div className="form-box-ctn">
-          <label htmlFor="">CHECK-OUT</label>
-          <input type="date" placeholder="Fecha de Check-out" />
-        </div>
+      <div className='form-box-ctn'>
+        <label htmlFor='dateout'>CHECK-OUT</label>
+        <input
+          name='dateout'
+          type='date'
+          placeholder='Fecha de Check-out'
+          onChange={(event) => handleChange(event)}
+          value={dateout}
+        />
+      </div>
 
-        <div className="form-box-ctn">
-          <label htmlFor="">GUESTS</label>
-          <input type="number" placeholder="Choose" />
-        </div>
+      <div className='form-box-ctn'>
+        <label htmlFor='guestnumber'>GUESTS</label>
+        <input
+          name='guestnumber'
+          type='number'
+          placeholder='Choose'
+          min='1'
+          max='5'
+          onChange={(event) => handleChange(event)}
+          value={guestnumber}
+        />
+      </div>
 
-        <div className="form-box-ctn">
-          <button className="search-btn">SEARCH</button>
-        </div>
-      </form>
+      <div className='form-box-ctn'>
+        <button className='search-btn'>SEARCH</button>
+      </div>
+    </form>
   );
 }
 
